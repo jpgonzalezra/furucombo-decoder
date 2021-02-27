96f110a392e4e0c707e32c78c108926b646b2b6a
Joaquin <jpgonzalezra@gmail.com>
2021-02-27 17:37:28 -0300

    test

--- src/herc20tokenin/decoder.test.ts
+++ src/herc20tokenin/decoder.test.ts
@@ -1,7 +1,7 @@
 import { execHErc20TokenIn } from './decoder';
 
 describe('furucombo herc20tokenin decoder', () => {
-  // inject(uint256,uint256,address[])                // 0xd0241dac
+  //  inject(uint256,uint256,address[])                // 0xd0241dac
   it.only('decode inject operation', async () => {
     const data = '';
     const result = execHErc20TokenIn(data);
